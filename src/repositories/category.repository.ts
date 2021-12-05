216a22ad54b845caff64d56e1c685ce05413b418
matheus <matheus.xmaz10@gmail.com>
2021-12-05 18:53:24 -0300

    validando o cadastro de categoria

--- src/repositories/category.repository.ts
+++ src/repositories/category.repository.ts
@@ -27,4 +27,9 @@ export class CategoriesRepository {
   list(): Category[] {
     return this.categories;
   }
+
+  findByName(name: string): Category {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
+  }
 }
