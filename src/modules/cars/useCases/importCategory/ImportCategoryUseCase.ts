76945a945fc0eef058b74a0f96b6b11c0f6cad8f
matheus <matheus.xmaz10@gmail.com>
2021-12-06 22:33:29 -0300

    adicionando multer

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,3 +1,7 @@
+import fs from "fs";
 export class ImportCategoryUseCase {
-  execute(file: Express.Multer.File) {}
+  execute(file: Express.Multer.File) {
+    const stream = fs.createReadStream(file.path);
+    stream.pipe()
+  }
 }
