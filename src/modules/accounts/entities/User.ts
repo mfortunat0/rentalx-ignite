6a21a8f8825cab5f916894912344fbb92fb7cd3f
matheus <matheus.xmaz10@gmail.com>
2021-12-17 20:19:06 -0300

    criando migration de usuario

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,33 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  driver_license: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
