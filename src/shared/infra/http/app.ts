0127d8c5c0de7989cf12398cf9107905c85c9145
matheus <matheus.xmaz10@gmail.com>
2021-12-23 20:52:47 -0300

    configurando supertest

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,28 @@
+import express, { NextFunction, Request, Response } from "express";
+import * as swaggerUi from "swagger-ui-express";
+import { router } from "./routes";
+import swaggerFile from "../../../swagger.json";
+import createConnection from "../typeorm";
+import "../../container";
+import { AppError } from "../../errors/AppError";
+import "express-async-errors";
+
+const app = express();
+createConnection();
+
+app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(router);
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({ message: err.message });
+    }
+    return response.status(500).json({
+      status: "erro",
+      message: `Internal server error ${err.message}`,
+    });
+  }
+);
+
+export { app };
